f6ee448a80c2bcc4dbc7bb3a67e6f760c96fcf21
Rashmi Verma <vermarashmi559@gmail.com>
2021-08-08 19:06:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import CSS from "./style.css"
+import Header from "./Header"
+import MemeGenerator from "./MemeGenerator"
+
+function App() {
+    return (
+        <div>
+       <Header />
+       <MemeGenerator />
+       </div>
+
+    )
+}
+export default App
